476449a309a351ceb37618fc5b708139f30ef5ee
gabsoares <soaresgabriel68@gmail.com>
2024-05-07 13:49:40 -0300

    Add project files.

---
+++ ManipulacaoArquivos/Product.cs
@@ -0,0 +1,23 @@
+﻿namespace ManipulacaoArquivos
+{
+    internal class Product
+    {
+        public int id { get; set; }
+        public string description { get; set; }
+        public double price { get; set; }
+        public int quantity { get; set; }
+
+        public Product(int id, string description, double price, int quantity)
+        {
+            Id = id;
+            this.description = description;
+            this.price = price;
+            this.quantity = quantity;
+        }
+
+        public override string? ToString()
+        {
+            return id+";"+description+";"+price+";"+quantity;
+        }
+    }
+}
---
+++ ManipulacaoArquivos/Program.cs
@@ -0,0 +1,45 @@
+﻿using System.Threading.Channels;
+
+namespace ManipulacaoArquivos
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            //Pega o caminho da pasta especial do usuario
+            //string path = Environment.GetFolderPath(Environment.SpecialFolder.UserProfile)+"\\Dados\\";
+            string path = @"C:\DadosEx\";
+            string file = "arquivo.txt";
+
+            //Se o caminho não existe, ele cria o caminho
+            if (!Directory.Exists(path))
+            {
+                Directory.CreateDirectory(path);
+            }
+
+            if (File.Exists(path + file))
+            {
+                StreamReader sr = new(path + file);
+                string s = sr.ReadToEnd();
+                Console.Clear();
+                Console.WriteLine(s);
+                sr.Close();
+
+                s += Console.ReadLine();
+
+                StreamWriter sw = new(path + file);
+                sw.WriteLine(s);
+                sw.Close();
+
+                Console.Clear();
+                sr = new(path + file);
+                Console.WriteLine(sr.ReadToEnd());
+
+                sr.Close();
+
+                var retorno = File.ReadLines(path + file).ElementAt(2);
+                Console.WriteLine(retorno);
+            }
+        }
+    }
+}
